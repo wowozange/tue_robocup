e83c1b9970c4976098a94ddc530abcecfb7b4890
Rein Appeldoorn <reinzor@gmail.com>
2017-03-30 12:16:39 +0200

    First clean cabinet and dinnertable, then look under

--- robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/r5cop_demo.py
+++ robocup_knowledge/src/robocup_knowledge/environments/robotics_testlabs/r5cop_demo.py
@@ -2,15 +2,15 @@ starting_point = "initial_pose"
 
 # required keys: entity_id (str), room_id (str), navigation_area (str), segment_areas (list)
 inspection_places = [
-    {"entity_id": "dinner_table",
-     "room_id": "livingroom",
-     "navigate_area": "near",
-     "segment_areas": ["on_top_of", "under"]},
-
     {"entity_id": "cabinet",
      "room_id": "kitchen",
      "navigate_area": "near",
      "segment_areas": ["on_top_of"]},
+
+    {"entity_id": "dinner_table",
+     "room_id": "livingroom",
+     "navigate_area": "near",
+     "segment_areas": ["on_top_of", "under"]},
 ]
 
 known_types = [
