d7416dbbb54ef1f773c86369fead04585442e127
Albert Hofkamp <a.t.hofkamp@tue.nl>
2019-11-02 13:23:01 +0100

    Enforce user request about the force sensor

--- robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
+++ robot_skills/examples/example_move_down_until_force_sensor_edge_up.py
@@ -4,6 +4,6 @@ from robot_skills.util.robot_constructor import robot_constructor
 
 rospy.init_node('test_mode_down_until_force_sensor_edge_up', anonymous=True)
 robot = robot_constructor("hero")
-arm = robot.get_arm()
+arm = robot.get_arm(available_force_sensor=True)
 
 arm.move_down_until_force_sensor_edge_up()
