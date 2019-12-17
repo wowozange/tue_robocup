9f816556807757d0d07791e1d0b1efa307d21437
Josja Geijsberts <j.geijsberts@student.tue.nl>
2019-12-17 20:59:22 +0100

    fix(smach_states)Fixed ShowOpenPose state

--- robot_smach_states/src/robot_smach_states/human_interaction/show_openpose.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/show_openpose.py
@@ -12,12 +12,14 @@ class ShowOpenPose(smach.State):
         self._openpose_subscriber = rospy.Subscriber('openpose/result_image', Image, self._callback)
         self._timeout = timeout
 
-    def execute(self, userdata):
-        try:
-            persons = self._robot.perception.detect_person_3d() # we should switch to openpose stream here
+    def execute(self, userdata=None):
+        # try:
+        rgb, depth, info = self._robot.perception.get_rgb_depth_caminfo(self._timeout)
+        self._robot.perception.detect_person_3d(rgb, depth, info)
+            # we should switch to openpose stream here
             # so next step is to develop a service that makes openpose switch between stream and service (and v.v.)
-        except:
-            return 'failed'
+        # except:
+        #     return 'failed'
         if self._image_openpose:
             self._robot.hmi.show_image_from_msg(self._image_openpose, self._timeout)
         return 'done'
