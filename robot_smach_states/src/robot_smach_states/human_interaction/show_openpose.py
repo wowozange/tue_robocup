b7ca1c231dae1d2e96fd9e0c64cf427c5731a97d
Josja Geijsberts <j.geijsberts@student.tue.nl>
2020-01-04 15:38:29 +0100

    fix(show_openpose): improved smach state implementation of showing openpose and implemented in grammar.

--- robot_smach_states/src/robot_smach_states/human_interaction/show_openpose.py
+++ robot_smach_states/src/robot_smach_states/human_interaction/show_openpose.py
@@ -1,29 +1,39 @@
 from sensor_msgs.msg import Image
 import smach
-import math
 import rospy
 
 
 class ShowOpenPose(smach.State):
-    def __init__(self, robot, timeout = 5.0):
+    def __init__(self, robot):
         smach.State.__init__(self, outcomes=['done', 'failed'])
         self._robot = robot
         self._image_openpose = None
         self._openpose_subscriber = rospy.Subscriber('openpose/result_image', Image, self._callback)
-        self._timeout = timeout
 
     def execute(self, userdata=None):
-        # try:
-        rgb, depth, info = self._robot.perception.get_rgb_depth_caminfo(self._timeout)
+        rgb, depth, info = self._robot.perception.get_rgb_depth_caminfo()
         self._robot.perception.detect_person_3d(rgb, depth, info)
-            # we should switch to openpose stream here
-            # so next step is to develop a service that makes openpose switch between stream and service (and v.v.)
-        # except:
-        #     return 'failed'
         if self._image_openpose:
-            self._robot.hmi.show_image_from_msg(self._image_openpose, self._timeout)
-        return 'done'
+            self._robot.hmi.show_image_from_msg(self._image_openpose)  # also parse seconds for duration?
+            return 'done'
+        else:
+            return 'failed'
 
     def _callback(self, data):
         rospy.logdebug('Received openpose result image')
         self._image_openpose = data
+
+
+class ShowOpenPoseLoop(smach.State):
+    def __init__(self, robot, iterations=10):
+        smach.State.__init__(self, outcomes=['done', 'failed'])
+        self._robot = robot
+        self._iterations = iterations
+
+    def execute(self, userdata=None):
+        state = ShowOpenPose(self._robot)
+        for i in range(self._iterations):
+            outcome = state.execute()
+            if outcome == 'failed':
+                return 'failed'
+        return 'done'
